89c0d0b44d21e76763fb81e3ee05921de487da3a
Iván Ortiz Martín <ivan@transgruasortiz.com>
2022-04-09 17:08:51 +0200

    feat: read operations .pdf and calculate taxes v2.0 - SPARK OPERATIONS

--- read_pdf.py
+++ read_pdf.py
@@ -1,13 +1,21 @@
 import pandas as pd
-from tabula import read_pdf
+import tabula
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
+from pyspark.sql.window import Window
 
+# SPARKSESSION
+spark = SparkSession\
+    .builder\
+    .appName("CryptoTaxes")\
+    .getOrCreate()
 
+# PANDAS VISUALIZATION OPTIONS
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_colwidth', None)
-
-
-df = read_pdf('./in/mov_coinbase_prueba.pdf', pages='all')[2:].iloc[:, 1:]
+# LOAD DATAFRAME OPERATIONS
+df = tabula.read_pdf('./in/mov_coinbase_prueba.pdf', pages='all')[2:].iloc[:, 1:]
 columns = df.iloc[0].tolist()
 df = df.iloc[1:]
 df.columns = columns
@@ -25,3 +33,14 @@ df_1.to_excel('./mov.xlsx')
 # df1_df = pd.DataFrame(df1)
 # df2_df = df1_df.sort_values(0)
 # df.columns
+
+# SPARK TREATMENT FOR AGGREGATE
+dfs_1 = spark.createDataFrame(df_1)
+dfs_1.show(4, False)
+dfs_1.printSchema()
+dfs_2 = dfs_1.groupby('Product').agg({'Total': 'sum'})\
+    .withColumnRenamed("sum(Total)", "sum_oper")
+dfs_2.agg({'sum_oper': 'sum'}).show()
+## OPTIONAL
+dfs_3 = dfs_2.filter(f.col('Product') != 'LTC-EUR')
+dfs_3.agg({'sum_oper': 'sum'}).show()
